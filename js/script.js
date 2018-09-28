0823228da69077000863e62c78ba170bd83be50e
Charlie Jaime <baroonie10@gmail.com>
2018-09-28 09:49:32 -0700

    all content layout is complete next styles, color scheme, responsiveness

--- js/script.js
+++ js/script.js
@@ -1,29 +1,4 @@
 $(document).ready(function($) {
-  //Create a select and append to #menu
-  var $select = $("<select></select>");
-  $("#navbar").append($select);
-
-  //Cycle over menu links
-  $("#navbar-links a").each(function(){
-    var $anchor = $(this);
-    //Create an option
-    var $option = $("<option></option>");
-
-    //option's value is the href
-    $option.val($anchor.attr("href"));
-    //option's text is the text of link
-    $option.text($anchor.text());
-    //append option to select
-    $select.append($option);
-  });
-
-  //Bind change listener to the select
-  $select.change(function(){
-  //Go to select's location
-  window.location = $select.val();
-  });
-
-
   $("a").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
