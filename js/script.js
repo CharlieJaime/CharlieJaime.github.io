08da1a39ce2ef44d53709efd2bb28773db7c3a4c
Charlie Jaime <CharlieJaimeDev@gmail.com>
2022-08-22 22:27:58 -0700

    removed all display trying slider content

--- js/script.js
+++ js/script.js
@@ -26,4 +26,19 @@ $(document).ready(function($) {
       });
     } // End if
   });
+
+  const panels = document.querySelectorAll('.panel')
+
+  panels.forEach((panel)=>{
+    panel.addEventListener('click', ()=>{
+      removeActiveClasses()
+      panel.classList.add('active')
+    })
+  })
+
+  function removeActiveClasses(){
+    panels.forEach(panel => {
+      panel.classList.remove('active');
+    })
+  }
 });
